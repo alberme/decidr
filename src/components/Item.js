544192fdf4c5d4db9b29ab9d8f5aad150868ee62
alberme <albertmartinez16@gmail.com>
2021-12-06 09:30:04 -0800

    fix(Item): remove console.log

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,11 +3,7 @@ import { MdClose } from "react-icons/md";
 export default function Item ({ children, id, onRemove }) {
   return (
     <div className="list-item-container">
-      <p
-        className="list-item-text"
-        onMouseEnter={() => console.log('entered')}
-        onMouseLeave={() => console.log('leave')}
-        >
+      <p className="list-item-text">
           {children}
       </p>
       <MdClose onClick={() => onRemove(id)}/>
