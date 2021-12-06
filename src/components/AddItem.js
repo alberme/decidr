d98801aae8f384e0dcba073608e3656905e452e3
alberme <albertmartinez16@gmail.com>
2021-12-06 00:47:18 -0800

    feat(error checks): add error checking #11

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,24 +1,38 @@
 import { useState } from 'react';
 
-export default function AddItem({ setListContainer, setSelectedItem }) {
+export default function AddItem({ setListContainer, setSelectedItem, listContainer }) {
   const [newItem, setNewItem] = useState('');
+  const [error, setError] = useState('');
 
   const formSubmit = (event) => {
     event.preventDefault();
-    setListContainer(listContainer => [ ...listContainer, newItem ]);
-    setNewItem('');
+    const itemNotBlank = newItem.trim().length > 0
+    const hasNoDuplicates = listContainer.every(item => item.toLowerCase() !== newItem.toLowerCase());
+
+    if (itemNotBlank && hasNoDuplicates) {
+      setListContainer(listContainer => [ ...listContainer, newItem.trim() ]);
+      setNewItem('');
+    } else if (!itemNotBlank) {
+      setError('Please enter an item');    
+    } else if (!hasNoDuplicates) {
+      setError('That item is a duplicate');
+    }
   }
   return (
     <form className="form-add-item">
-      <input
-        type="text"
-        placeholder="Type here!"
-        value={newItem}
-        onChange={event => {
-          setNewItem(event.target.value)
-          setSelectedItem('');
-        }}
-      />
+      <div className="form-input-container">
+        <input
+          type="text"
+          placeholder="Type here!"
+          value={newItem}
+          onChange={event => {
+            setNewItem(event.target.value)
+            setSelectedItem('');
+            setError('');
+          }}
+        />
+        { error && <p className="error-text">{error}</p> }
+      </div>
       <button
         type="submit"
         onClick={formSubmit}
